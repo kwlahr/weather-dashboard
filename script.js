3d133213aec68842f9b475749d59fde28b635999
kwlahr <kwlahr@gmail.com>
2019-10-18 15:26:43 -0600

    added dynamic anchor tag to response.name for click functionality once 5 day forcast api is integrated

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
 window.onload = function() {
-//   var data = $(queryURL).data(data);
 // var country = document.getElementById("countryName", "text");
-// var search = document.getElementById("search");
-// var search = $("search").on("click")
 var city = "denver";
 var country = "us";
   // var city = $("#cityName").val();
   // var country = $("#countryName").val();
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&APPID=e118a9757bbd90e8cbd8a9d2eff8445f";
   var today = new Date();
-
+  
   var simpleDate = (today.getMonth()+1) + "/" + today.getDate() + "/" + today.getFullYear();
   // console.log(queryURL);
   
-  $.ajax({
-      url: queryURL,
+  var latitude = "37.75";
+  var longitude = "-122.37";
+  var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=e118a9757bbd90e8cbd8a9d2eff8445f";
+
+  $("#search").on("click", function(){
+  
+    $.ajax({
+      url: uvURL,
       method: "GET"
     }).then(function(response) {
-    // console.log(response);
-    // console.log(response.name);
-    // console.log(response.weather[0].icon);
-    // console.log(response.main.temp);
-    // console.log(response.main.humidity);
-    // console.log(response.wind.speed);
-    $("#search").on("click", function(){
+    // var latitude = response.coord.lat;
+    // var longitude = response.coord.lon;
+    console.log(response);
+    console.log(uvURL);
+    $(".uv").append("Current UV Index: " + response.value)
+    });
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    
       console.log(response);
       var farenheit = (response.main.temp - 273.15) * 9/5 + 32;
-      $(".city").append("City: " + response.name);
+      // var iconCode = "10d";
+      var iconURL = "http://openweathermap.org/img/w/10d.png";
+      $(".city").append("City: " + "<a href='#'>" + response.name + "</a>");
+      $('#wicon').append("<img id='wicon' src='" + iconURL + "'" + ">");
       $(".date").append("Today: " + simpleDate);
       $(".temp").append("Current temperature: " + Math.round(farenheit) + " &#xb0;F");
-      $(".humidity").append("Current humidity: " + response.main.humidity);
-      $(".windSpeed").append("Current wind speed: " + response.wind.speed);
+      $(".humidity").append("Current humidity: " + response.main.humidity + "%");
+      $(".windSpeed").append("Current wind speed: " + response.wind.speed + " MPH");
+      
     });
+
   });
   
 
